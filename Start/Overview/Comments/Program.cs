d5fa71140fd8d125826df646e56d735e42305e88
Dany <signups.dany@gmail.com>
2024-09-09 14:38:55 -0500

    up to chapter 4

---
+++ Start/Overview/Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax 
+        /// <summary>
+        /// XML Comments are used to help provide documentation
+        /// </summary>
+        /// <param name="args">An Array of string arguments from the command line</param>  
+        /// <returns> No return value </returns>
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
